d2844c9d68a16d5305ad6d891bb7faa172fd8519
langlk <kels.langlois@gmail.com>
2017-05-18 13:39:15 -0700

    Update Set1Challenge3.py

--- Set1Challenge3.py
+++ Set1Challenge3.py
@@ -48,7 +48,7 @@ def binary_xOR(str1,str2): # takes two binary code strings, returns XOR of them 
         a += 1
     return newcode
     
-def bin_to_ascii(bin_code):
+def bin_to_ascii(bin_code): # changes binary code string to ascii string
   i = 0
   ascii_string = ""
   while i+8 <= len(bin_code):
@@ -56,53 +56,70 @@ def bin_to_ascii(bin_code):
     i += 8
   return ascii_string
   
-def ascii_to_bin(ascii_string):
-  
+def ascii_to_bin(ascii_string): # changes ascii string to binary string
+  bin_code = ""
+  for char in ascii_string:
+    temp_code = str(bin(ord(char)))
+    temp_code = temp_code[2:len(temp_code)] # Removes the binary prefix
+    while len(temp_code) < 8: # Standardizes length
+      temp_code = "0" + temp_code
+    bin_code += temp_code
+  return bin_code
 
 def frequency_score(code):
+  """
+  Scores letter frequency of an ascii string. Starts with code_score = 1, then for each character in the code, multiplies the code_score by the frequency of that letter. If not a space or a letter, multiplies by .0001 to penalize weird characters. 
+  Higher Frequency Score is better.
+  Only works for strings of the same length.
+  """
   letter_frequencies = {
-    ' ': 18.28,
-    'e': 10.26, 
-    't': 7.52,
-    'a': 6.53,
-    'o': 6.15,
-    'n': 5.71,
-    's': 5.67,
-    'r': 4.99,
-    'h': 4.98,
-    'd': 3.28,
-    'l': 3.31,
-    'u': 2.27,
-    'c': 2.23,
-    'm': 2.03,
-    'f': 1.98,
-    'w': 1.70,
-    'g': 1.62,
-    'p': 1.50,
-    'y': 1.42,
-    'b': 1.26,
-    'v': 0.80,
-    'k': 0.56,
-    'x': 0.14,
-    'j': 0.10,
-    'q': 0.08,
-    'z': 0.05
+    ' ': .1828,
+    'e': .1026, 
+    't': .0752,
+    'a': .0653,
+    'o': .0615,
+    'n': .0571,
+    's': .0567,
+    'r': .0499,
+    'h': .0498,
+    'd': .0328,
+    'l': .0331,
+    'u': .0227,
+    'c': .0223,
+    'm': .0203,
+    'f': .0198,
+    'w': .0170,
+    'g': .0162,
+    'p': .0150,
+    'y': .0142,
+    'b': .0126,
+    'v': 0.0080,
+    'k': 0.0056,
+    'x': 0.0014,
+    'j': 0.0010,
+    'q': 0.0008,
+    'z': 0.0005
   }
-  code_frequencies = {}
-  for char in code.lower():
-    if char in code_frequencies:
-      code_frequencies[char] += 1
-    else:
-      code_frequencies[char] = 1
-  score = 0
-  for char in code_frequencies.keys():
+  code_score = 1
+  for char in code.lower(): # Checks as lowercase
     if char in letter_frequencies:
-      score += abs(letter_frequencies[char] - 100*(code_frequencies[letter]/len(code)))
-    else:
-      score += 100*(code_frequencies[letter]/len(code))
-  return score
+      code_score *= letter_frequencies[char]
+    else: # Some random character/punctuation
+      code_score *= 0.0001
+  return code_score
 
-code = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-bin_code_1 = hex_to_binary(code)
-for a in range(255):
-  test_string = chr(a)*len(code)
+start_code = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+bin_code_string = hex_to_binary(start_code)
+scores = {} # Stores scores of each character xOR'd against
+"""
+XORs the code against string of one character.
+Then changes back to an ascii string, scores the character frequency, and stores.
+"""
+for a in range(255): # not sure if only checking through 255 will work for ch6
+  test_string = chr(a)*len(start_code)
+  test_string = ascii_to_bin(test_string)
+  result_string = binary_xOR(bin_code_string,test_string)
+  result_string = bin_to_ascii(result_string)
+  scores[frequency_score(result_string)] = [chr(a),result_string] 
+  # storing the scores as the keys of this dictionary right now, but this is probably a terrible idea if two codekeys ever produce the same score
+print(scores[max(scores.keys())]) 
