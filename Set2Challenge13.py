8a08500ee0f9feade6121ba7bd291be734a5cacd
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-23 18:03:10 -0700

    Add function to create a profile represented as a string.

--- Set2Challenge13.py
+++ Set2Challenge13.py
@@ -1,6 +1,8 @@
 # Cryptopals
 # Set 2 Challenge 13
 
+from random import randint
+
 def parse_key_val(key_val_string):
     key_val_list = key_val_string.split("&")
     key_val_obj = {}
@@ -8,3 +10,12 @@ def parse_key_val(key_val_string):
         key_val = pair.split("=")
         key_val_obj[key_val[0]] = key_val[1]
     return key_val_obj
+
+def profile_for(email):
+    if (email.find("&") != -1 or email.find("=") != -1):
+        return "Invalid Email"
+    else:
+        return "email=" + email + "&uid=10&role=user"
+
+print(profile_for("kels@mail.com"))
+print(profile_for("kels&@mail.com"))
