f9c10293e99a6fb4f782f8c6a330e5a888b0053a
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-26 17:32:22 -0700

    Set up challenge 14 oracle function.

---
+++ Set2Challenge14.py
@@ -0,0 +1,42 @@
+# Cryptopals
+# Set 2 Challenge 14
+
+import sys
+import base64
+from Crypto.Cipher import AES
+from random import randint
+
+# Decrypt with AES in ECB
+def AES_ECB_decrypt(bytes_code, key):
+    aes_cipher = AES.new(key, AES.MODE_ECB)
+    return aes_cipher.decrypt(bytes_code)
+
+# Encrypt with AES in ECB
+def AES_ECB_encrypt(bytes_code, key):
+    aes_cipher = AES.new(key, AES.MODE_ECB)
+    return aes_cipher.encrypt(bytes_code)
+
+def pad_PKCS_7(message, blocksize):
+    padding_amount = blocksize - len(message) % blocksize
+    if padding_amount == 0:
+        padding_amount = blocksize
+    padding = bytes([padding_amount]) * padding_amount
+    return message + padding
+
+def random_bytes(n):
+    random_ints = []
+    for i in range(n):
+        random_ints.append(randint(0,255))
+    return bytes(random_ints)
+
+RANDOM_KEY = random_bytes(16)
+RANDOM_PADDING = random_bytes(randint(5,10))
+
+def random_encode(message):
+    message = pad_PKCS_7(RANDOM_PADDING + message, 16)
+    return AES_ECB_encrypt(message, RANDOM_KEY)
+
+def oracle_function(message):
+    b64_code = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
+    bytes_code = base64.b64decode(b64_code)
+    return random_encode(message + bytes_code)
