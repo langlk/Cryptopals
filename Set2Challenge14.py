4b7831e8622f3789cd7e7159783ded9a7b44f04b
Kelsey Langlois <kels.langlois@gmail.com>
2017-08-27 10:25:22 -0700

    Update S2C4 to find where unknown text is appended and align to run byte-at-a-time ECB decrypt

--- Set2Challenge14.py
+++ Set2Challenge14.py
@@ -1,7 +1,6 @@
 # Cryptopals
 # Set 2 Challenge 14
 
-import sys
 import base64
 from Crypto.Cipher import AES
 from random import randint
@@ -40,3 +39,32 @@ def oracle_function(message):
     b64_code = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
     bytes_code = base64.b64decode(b64_code)
     return random_encode(message + bytes_code)
+
+def get_duplicate(message, blocksize):
+    blocks = {}
+    for i in range(0, len(message), blocksize):
+        block = message[i:i + blocksize]
+        if block in blocks:
+            return block
+        else:
+            blocks[block] = 1
+    return False
+
+def find_duplicate_end(message, zero_block, blocksize):
+    for i in range(len(message) - blocksize, 0, -blocksize):
+        block = message[i: i + blocksize]
+        if block == zero_block:
+            return i + blocksize
+test_bytes = b'0' * 100
+encrypted_message = oracle_function(test_bytes)
+zero_block_encrypted = get_duplicate(encrypted_message, 16)
+print(zero_block_encrypted)
+# we want to find the first block after the random prefix that isn't a zero_block
+end_duplicate = find_duplicate_end(encrypted_message, zero_block_encrypted, 16)
+for i in range(1, 16):
+    test_bytes = test_bytes + (b'0' * i)
+    encrypted_message = oracle_function(test_bytes)
+    new_end_duplicate = find_duplicate_end(encrypted_message, zero_block_encrypted, 16)
+    if new_end_duplicate > end_duplicate:
+        end_duplicate = new_end_duplicate
+        break
