0758661a0b728ac31d3ee8bf6855849502010937
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-21 14:52:51 -0800

    Complete challenge 22

--- Set3/Challenge22.py
+++ Set3/Challenge22.py
@@ -7,8 +7,30 @@ import MersenneTwister
 import time
 import random
 
+# Impatient version
+
+start_time = int(time.time())
+seed = start_time + random.randint(40, 1000)
+twister = MersenneTwister.MersenneTwister(seed)
+result = twister.extract_number()
+print(result)
+
+for i in range(2050):
+    twist = MersenneTwister.MersenneTwister(start_time + i)
+    if twist.extract_number() == result:
+        print("Seed is " + str(start_time + i))
+
+# Patience-required version
+
 time.sleep(random.randint(40, 1000))
 seed = int(time.time())
-twister = MersenneTwister(seed)
+twister = MersenneTwister.MersenneTwister(seed)
 time.sleep(random.randint(40, 1000))
-print(twister.extract_number())
+result = twister.extract_number()
+print(result)
+
+end_time = int(time.time())
+for i in range(2050):
+    twist = MersenneTwister.MersenneTwister(end_time - i)
+    if twist.extract_number() == result:
+        print("Seed is " + str(end_time - i))
