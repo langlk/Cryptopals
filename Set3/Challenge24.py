f510ad15b7235fe643e5e725fef637fb1fffa762
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-23 14:06:45 -0800

    Add solution for password reset token section.

--- Set3/Challenge24.py
+++ Set3/Challenge24.py
@@ -5,6 +5,7 @@
 
 import MersenneTwister
 import random
+import time
 
 def binary_xOR(byte_code1,byte_code2):
     result = b''
@@ -23,6 +24,7 @@ def MT_stream_cipher(bytes_text, seed):
         output += binary_xOR(textblock, keystream)
     return output
 
+# picks random 16-bit seed for encoding, adds random gibberish to front of message, and encodes with MT stream
 def encode(message):
     seed = random.randint(0, 65536)
     for i in range(random.randint(5, 50)):
@@ -30,10 +32,37 @@ def encode(message):
     byte_message = message.encode('utf-8')
     return MT_stream_cipher(byte_message, seed)
 
+def password_reset():
+    seed = int(time.time())
+    message = 'password reset'
+    for i in range(random.randint(5, 50)):
+        message = chr(random.randint(0, 256)) + message
+    for i in range(random.randint(5, 50)):
+        message += chr(random.randint(0, 256))
+    byte_message = message.encode('utf-8')
+    return MT_stream_cipher(byte_message, seed)
+
+# Part one: finding seed from known plaintext.
 secret_message = encode('A' * 14)
 
+# Brute force Huzzah! (Questionmark?)
+# Just checks every possible seed till it finds the known plaintext
 for i in range(65536):
     output = str(MT_stream_cipher(secret_message, i))
     if 'A' * 14 in output:
         print("Seed is:", i)
         print("Decoded:", output)
+        break
+
+# Part two: making password reset token
+
+reset_token = password_reset()
+
+now = int(time.time())
+
+for i in range(2000):
+    output = str(MT_stream_cipher(reset_token, now - i))
+    if 'password' in output:
+        print("Seed is:", now - i)
+        print("Decoded:", output)
+        break
